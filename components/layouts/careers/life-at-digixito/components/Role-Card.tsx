5ede7358ac6d5c65ed046242b4d9cd382be9cdf8
Kunal Sharma <kunalpandit5200@gmail.com>
2026-03-02 12:31:50 +0530

    feat: create RoleCard component for displaying role information.

---
+++ components/layouts/careers/life-at-digixito/components/Role-Card.tsx
@@ -0,0 +1,12 @@
+import { Box, Card } from "@mui/material";
+import React from "react";
+
+const RoleCard = () => {
+  return (
+    <Box>
+      <Card sx={{ boxShadow: "none" }}></Card>
+    </Box>
+  );
+};
+
+export default RoleCard;
